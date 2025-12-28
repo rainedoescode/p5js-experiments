0fa14e357345299b538542422213ba954fad21cb
Raine Adams <rainedoescode@gmail.com>
2025-12-28 09:01:31 -0600

    Create functions to fill canvas with random pixels

--- random_pixels/sketch.js
+++ random_pixels/sketch.js
@@ -4,4 +4,25 @@ function setup() {
 
 function draw() {
   background(220);
+  fillRandomColors();
+  noLoop(); // Stop draw from looping
+}
+
+// Function to fill the entire canvas with random colors
+function fillRandomColors() {
+  
+  loadPixels();
+  for (let i = 0; i < pixels.length; i += 4) {
+    let c = randomRGBColor();
+    pixels[i] = red(c);
+    pixels[i + 1] = green(c);
+    pixels[i + 2] = blue(c);
+    pixels[i + 3] = 255; // no transparency
+  }
+  updatePixels();
+}
+
+// Helper function to generate a random RGB color
+function randomRGBColor() {
+  return color(random(255), random(255), random(255));
 }
